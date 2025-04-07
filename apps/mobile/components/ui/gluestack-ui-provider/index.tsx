7622c6f64af585e4cb0b0e17ea176cd41afdfbc7
Julius Pahama <pahamajulius@gmail.com>
2025-04-07 12:33:43 +0800

    refactor(mobile-theme): Refactor Tailwind CSS configuration and clean up unused CSS variables; improve code readability

--- apps/mobile/components/ui/gluestack-ui-provider/index.tsx
+++ apps/mobile/components/ui/gluestack-ui-provider/index.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import { config } from './config';
-import { View, ViewProps } from 'react-native';
 import { OverlayProvider } from '@gluestack-ui/overlay';
 import { ToastProvider } from '@gluestack-ui/toast';
 import { useColorScheme } from 'nativewind';
+import React, { useEffect } from 'react';
+import { View, ViewProps } from 'react-native';
+import { config } from './config';
 import { ModeType } from './types';
 
 export function GluestackUIProvider({
@@ -22,13 +22,7 @@ export function GluestackUIProvider({
   }, [mode]);
 
   return (
-    <View
-      style={[
-        config[colorScheme!],
-        { flex: 1, height: '100%', width: '100%' },
-        props.style,
-      ]}
-    >
+    <View style={[config[colorScheme!], { flex: 1, height: '100%', width: '100%' }, props.style]}>
       <OverlayProvider>
         <ToastProvider>{props.children}</ToastProvider>
       </OverlayProvider>
