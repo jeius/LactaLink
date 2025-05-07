0434eddee889b45685a0ec97c9749aee58769598
Julius Pahama <pahamajulius@gmail.com>
2025-05-07 09:18:03 +0800

    refactor(mobile): simplify Protected component styling and update build command

--- apps/mobile/components/protected/index.tsx
+++ apps/mobile/components/protected/index.tsx
@@ -1,21 +1,16 @@
 import { useSession } from '@/hooks/useSession';
 import { getHexColor } from '@/lib/colors';
 import { userHasProfile } from '@/lib/utils/userHasProfile';
-import { tva } from '@gluestack-ui/nativewind-utils';
+import { cn } from '@gluestack-ui/nativewind-utils/cn';
 import { router } from 'expo-router';
 import { useEffect } from 'react';
 import { SafeAreaView, SafeAreaViewProps } from 'react-native-safe-area-context';
 import { useTheme } from '../providers/theme-provider';
 import { Spinner } from '../ui/spinner';
 
-const baseStyle = tva({
-  base: 'bg-background-50 relative flex-1 items-center justify-center',
-});
-
 export function Protected({ className, ...props }: SafeAreaViewProps) {
   const { user, session, isLoading } = useSession();
   const { theme } = useTheme();
-
   useEffect(() => {
     if (!isLoading) {
       if (!user || !session) {
@@ -29,11 +24,16 @@ export function Protected({ className, ...props }: SafeAreaViewProps) {
 
   if (isLoading || !user || !session) {
     return (
-      <SafeAreaView className={baseStyle()}>
+      <SafeAreaView className="bg-background-50 relative flex-1 items-center justify-center">
         <Spinner color={getHexColor(theme, 'primary', 500)} size={'large'} />
       </SafeAreaView>
     );
   }
 
-  return <SafeAreaView className={baseStyle({ class: className })} {...props} />;
+  return (
+    <SafeAreaView
+      className={cn('bg-background-50 relative flex-1 items-center justify-center', className)}
+      {...props}
+    />
+  );
 }
