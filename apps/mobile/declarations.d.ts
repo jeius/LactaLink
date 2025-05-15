dc83c303b7f048ab99f875d9c1f73f15c40aed10
Julius Pahama <pahamajulius@gmail.com>
2025-05-15 09:02:15 +0800

    Add new assets and update app configuration
    - Updated app.json to include specific assets for expo-asset plugin.
    - Added new SVG icon for information.
    - Introduced multiple new PNG images for various app functionalities:
      - admin.png
      - forgot-password.png
      - hospital.png
      - individual.png
      - milk-bank.png
      - onboarding1.png
      - onboarding2.png
      - onboarding3.png
      - onboarding4.png
      - sign-in.png
      - sign-up.png
      - verification.png

--- apps/mobile/declarations.d.ts
+++ apps/mobile/declarations.d.ts
@@ -13,3 +13,9 @@ declare module '*.svg' {
   const content: React.FC<SvgProps>;
   export default content;
 }
+
+declare module '*.png' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const value: string;
+  export default value;
+}
