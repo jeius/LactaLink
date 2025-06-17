06a6008b43a4f9fceae37e7633628901c1a33db0
Julius Pahama <pahamajulius@gmail.com>
2025-06-18 07:39:22 +0800

    feat(workflow): add Android and iOS Maps API keys to .env file in build preview workflow

--- apps/mobile/app/(root)/(tabs)/home/index.tsx
+++ apps/mobile/app/(root)/(tabs)/home/index.tsx
@@ -2,16 +2,13 @@ import { Protected } from '@/components/Protected';
 import ThemeToggler from '@/components/ThemeToggler';
 import { Button, ButtonIcon, ButtonText } from '@/components/ui/button';
 import { VStack } from '@/components/ui/vstack';
-import { LogOutIcon, MapPinIcon } from 'lucide-react-native';
-
-import * as crypto from 'expo-crypto';
+import { LogOutIcon } from 'lucide-react-native';
 
 import { signOut } from '@/auth';
-import { delay, extractErrorMessage } from '@lactalink/utilities';
+import { extractErrorMessage } from '@lactalink/utilities';
 import React from 'react';
 import { toast } from 'sonner-native';
 
-import ComboBox from '@/components/ComboBox';
 import { useRouter } from 'expo-router';
 
 const Home = () => {
@@ -42,55 +39,6 @@ const Home = () => {
         >
           <ButtonText>Go to setup profile</ButtonText>
         </Button>
-
-        <Button
-          action="primary"
-          onPress={async () => {
-            toast.promise(delay(2000), {
-              loading: 'Loading promise toast',
-              success: () => 'Promise toast resolved',
-              error: 'Promise toast rejected',
-            });
-          }}
-        >
-          <ButtonText>Show promise toast</ButtonText>
-        </Button>
-
-        <Button
-          action="positive"
-          onPress={() => {
-            toast.success('Success', { id: crypto.randomUUID() });
-          }}
-        >
-          <ButtonText>Show success toast</ButtonText>
-        </Button>
-
-        <Button
-          action="default"
-          onPress={() => {
-            toast.info('Info', { id: crypto.randomUUID() });
-          }}
-        >
-          <ButtonText>Show info toast</ButtonText>
-        </Button>
-
-        <Button
-          action="negative"
-          onPress={() => {
-            toast.error('Error', { id: crypto.randomUUID() });
-          }}
-        >
-          <ButtonText>Show error toast</ButtonText>
-        </Button>
-
-        <ComboBox
-          collection="barangays"
-          searchPath="name"
-          labelPath="code"
-          descriptionPath="name"
-          icon={MapPinIcon}
-          iconPosition="left"
-        />
       </VStack>
     </Protected>
   );
