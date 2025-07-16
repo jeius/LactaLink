1eb83b68e12370222bed28ff4d7103b14a8ec3f6
Julius Pahama <pahamajulius@gmail.com>
2025-07-16 17:00:56 +0800

    feat(GooglePlacesInput): enhance location selection with detailed data and customizable input styles

--- apps/mobile/components/GooglePlacesInput.tsx
+++ apps/mobile/components/GooglePlacesInput.tsx
@@ -1,29 +1,64 @@
 import { getHexColor } from '@/lib/colors';
 import { ANDROID_MAPS_API_KEY, IOS_MAPS_API_KEY } from '@/lib/constants';
-import { LucideSearchX } from 'lucide-react-native';
+import { getImageAsset } from '@/lib/stores';
+import { RefObject } from 'react';
 import { Platform } from 'react-native';
 import {
   GooglePlaceData,
   GooglePlaceDetail,
   GooglePlacesAutocomplete,
+  GooglePlacesAutocompleteProps,
+  GooglePlacesAutocompleteRef,
 } from 'react-native-google-places-autocomplete';
 import { LatLng } from 'react-native-maps';
 import { useTheme } from './AppProvider/ThemeProvider';
-import { Icon } from './ui/icon';
+import { Image } from './Image';
 import { Spinner } from './ui/spinner';
 import { Text } from './ui/text';
 import { VStack } from './ui/vstack';
 
-interface SearchPlacesProps {
-  onSelected?: (value: { location: LatLng }) => void;
+export type LocationDetails = {
+  location: LatLng;
+  googlePlaceData: GooglePlaceData;
+  googlePlaceDetail: GooglePlaceDetail | null;
+};
+
+interface SearchPlacesProps
+  extends Pick<GooglePlacesAutocompleteProps, 'renderLeftButton' | 'renderRightButton'> {
+  onSelected?: (value: LocationDetails) => void;
+  rounded?: 'sm' | 'md' | 'lg' | 'full';
+  inputRef?: RefObject<GooglePlacesAutocompleteRef | null>;
 }
 
 const API_KEY = Platform.OS === 'ios' ? IOS_MAPS_API_KEY : ANDROID_MAPS_API_KEY;
 
-export function GooglePlacesInput({ onSelected }: SearchPlacesProps) {
+export function GooglePlacesInput({
+  onSelected,
+  rounded = 'md',
+  inputRef,
+  ...props
+}: SearchPlacesProps) {
   const { theme } = useTheme();
   const backgroundColor = getHexColor(theme, 'background', 0);
   const borderColor = getHexColor(theme, 'outline', 300);
+
+  let borderRadius: number;
+  switch (rounded) {
+    case 'sm':
+      borderRadius = 6;
+      break;
+    case 'md':
+      borderRadius = 10;
+      break;
+    case 'lg':
+      borderRadius = 14;
+      break;
+    case 'full':
+      borderRadius = 9999; // Full rounded
+      break;
+    default:
+      borderRadius = 10; // Default to md
+  }
 
   function handleSelect(data: GooglePlaceData, detail: GooglePlaceDetail | null) {
     if (detail?.geometry?.location) {
@@ -31,27 +66,35 @@ export function GooglePlacesInput({ onSelected }: SearchPlacesProps) {
         latitude: detail.geometry.location.lat,
         longitude: detail.geometry.location.lng,
       };
-      onSelected?.({ location });
+      onSelected?.({ location, googlePlaceData: data, googlePlaceDetail: detail });
     }
   }
 
   return (
     <GooglePlacesAutocomplete
+      ref={inputRef}
+      {...props}
       placeholder="Search for a Location"
       textInputProps={{
         selectionHandleColor: getHexColor(theme, 'primary', 500),
         placeholderTextColor: getHexColor(theme, 'typography', 500),
         cursorColor: getHexColor(theme, 'primary', 500),
         returnKeyType: 'search',
-        className: 'border flex-1 border-outline-500 text-sm px-3 py-0 text-typography-900',
       }}
       styles={{
         textInput: {
-          borderRadius: 10,
+          borderRadius,
+          borderColor: getHexColor(theme, 'outline', 500),
+          borderWidth: 1,
+          color: getHexColor(theme, 'typography', 900),
+          fontSize: 14,
           fontFamily: 'Jakarta-Regular',
           marginHorizontal: 4,
+          paddingVertical: 0,
+          paddingHorizontal: 12,
           height: 40,
           backgroundColor,
+          flex: 1,
         },
         listView: {
           backgroundColor,
@@ -61,7 +104,7 @@ export function GooglePlacesInput({ onSelected }: SearchPlacesProps) {
         },
         separator: { backgroundColor: borderColor },
         row: { padding: 0, backgroundColor: 'transparent' },
-        poweredContainer: { borderColor, backgroundColor: getHexColor(theme, 'background', 300) },
+        poweredContainer: { borderColor, backgroundColor: getHexColor(theme, 'background', 100) },
       }}
       onPress={handleSelect}
       query={{
@@ -71,12 +114,12 @@ export function GooglePlacesInput({ onSelected }: SearchPlacesProps) {
       }}
       enablePoweredByContainer={true}
       predefinedPlaces={[]}
-      autoFillOnNotFound={false}
+      autoFillOnNotFound={true}
       currentLocation={false}
       currentLocationLabel="Current location"
-      debounce={0}
+      debounce={200}
       disableScroll={false}
-      enableHighAccuracyLocation={true}
+      enableHighAccuracyLocation={false}
       fetchDetails={true}
       filterReverseGeocodingByTypes={[]}
       GooglePlacesDetailsQuery={{ rankby: 'distance' }}
@@ -91,7 +134,7 @@ export function GooglePlacesInput({ onSelected }: SearchPlacesProps) {
       renderRow={RenderRow}
       listViewDisplayed={'auto'}
       keepResultsAfterBlur={false}
-      minLength={2}
+      minLength={1}
       nearbyPlacesAPI="GooglePlacesSearch"
       numberOfLines={1}
       onFail={(e) => {
@@ -110,9 +153,14 @@ export function GooglePlacesInput({ onSelected }: SearchPlacesProps) {
 
 function ListEmpty() {
   return (
-    <VStack space="lg" className="mx-auto items-center p-4">
-      <Icon as={LucideSearchX} className="text-primary-500 h-12 w-12" />
-      <Text size="md">No results found.</Text>
+    <VStack className="mx-auto items-center p-4">
+      <Image
+        alt="No Data"
+        source={getImageAsset('noData_0.75x')}
+        style={{ width: 80, aspectRatio: 1 }}
+        contentFit="contain"
+      />
+      <Text className="font-JakartaMedium">No results found.</Text>
     </VStack>
   );
 }
