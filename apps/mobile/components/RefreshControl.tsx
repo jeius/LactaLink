3b5cf74b83ec4a596257a054f8848ba7173670a7
Julius Pahama <pahamajulius@gmail.com>
2025-07-22 23:36:05 +0800

    feat: enhance RefreshControl to support custom onRefresh handling

--- apps/mobile/components/RefreshControl.tsx
+++ apps/mobile/components/RefreshControl.tsx
@@ -1,17 +1,30 @@
+import { useRevalidateQueries } from '@/hooks/collections/useRevalidateQueries';
 import { getHexColor } from '@/lib/colors';
 import { FC } from 'react';
 import { RefreshControlProps, RefreshControl as RNRefreshControl } from 'react-native';
 import { useTheme } from './AppProvider/ThemeProvider';
 
-export const RefreshControl: FC<RefreshControlProps> = (props) => {
+export const RefreshControl: FC<RefreshControlProps> = ({ onRefresh, ...props }) => {
   const { theme } = useTheme();
+  const revalidateQueries = useRevalidateQueries();
+
   const tintColor = getHexColor(theme, 'primary', 500);
   const bgColor = getHexColor(theme, 'background', 50);
+
+  function handleRefresh() {
+    if (onRefresh) {
+      onRefresh();
+    } else {
+      revalidateQueries();
+    }
+  }
+
   return (
     <RNRefreshControl
       {...props}
       colors={tintColor ? [tintColor] : undefined}
       progressBackgroundColor={bgColor}
+      onRefresh={handleRefresh}
     />
   );
 };
