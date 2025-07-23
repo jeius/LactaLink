3cd5a542468edddc984c5e2b5df42eda734de7a3
Julius Pahama <pahamajulius@gmail.com>
2025-07-23 11:47:19 +0800

    feat: enhance useRevalidateQueries to support selective query invalidation

--- apps/mobile/hooks/collections/useRevalidateQueries.ts
+++ apps/mobile/hooks/collections/useRevalidateQueries.ts
@@ -5,15 +5,24 @@ import { useCallback } from 'react';
 export function useRevalidateQueries() {
   const queryClient = useQueryClient();
 
-  const revalidateQueries = useCallback(() => {
-    queryClient.invalidateQueries({
-      predicate(query) {
-        return [...COLLECTION_QUERY_KEY, ...INFINITE_QUERY_KEY, ...QUERY_KEYS.AUTH.USER].some(
-          (key) => query.queryKey.includes(key)
-        );
-      },
-    });
-  }, [queryClient]);
+  const revalidateQueries = useCallback(
+    (select: 'all' | 'collections' | 'user' = 'all') => {
+      const keysToInvalidate = {
+        all: [...COLLECTION_QUERY_KEY, ...INFINITE_QUERY_KEY, ...QUERY_KEYS.AUTH.USER],
+        collections: COLLECTION_QUERY_KEY,
+        user: QUERY_KEYS.AUTH.USER,
+      }[select];
+
+      if (!keysToInvalidate) return;
+
+      queryClient.invalidateQueries({
+        predicate(query) {
+          return keysToInvalidate.some((key) => query.queryKey.includes(key));
+        },
+      });
+    },
+    [queryClient]
+  );
 
   return revalidateQueries;
 }
