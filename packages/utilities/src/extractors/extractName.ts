f89cfc9827bfd7e55cb442dbb409864c7b399b66
Julius Pahama <pahamajulius@gmail.com>
2025-08-06 16:00:59 +0800

    refactor(utilities): update extractName function to accept a more specific user type

--- packages/utilities/src/extractors/extractName.ts
+++ packages/utilities/src/extractors/extractName.ts
@@ -6,7 +6,7 @@ import { User } from '@lactalink/types';
  * @param user - The user object from which to extract the name.
  * @returns {string | null} - The extracted name from the user profile, or null if not found.
  */
-export function extractName(user: User): string | null {
+export function extractName(user: Pick<User, 'profile'>): string | null {
   if (user.profile) {
     const profile = user.profile.value;
     if (typeof profile === 'object') {
