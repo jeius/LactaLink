ef68853314ff1d17e2993e4dcf173315e5d05796
Julius Pahama <pahamajulius@gmail.com>
2025-08-12 08:47:30 +0800

    feat: Add lerpAngle function for interpolating angles

--- packages/utilities/src/geo-utils/index.ts
+++ packages/utilities/src/geo-utils/index.ts
@@ -12,3 +12,8 @@ export {
 export * from './converters';
 export * from './spatialSearch';
 export * from './validatePoint';
+
+export function lerpAngle(a: number, b: number, t: number) {
+  let diff = ((b - a + 540) % 360) - 180;
+  return (a + diff * t + 360) % 360;
+}
