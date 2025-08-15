ace5b9b3690b7348bc8524e50068c01ed27898c1
Julius Pahama <pahamajulius@gmail.com>
2025-08-15 16:30:41 +0800

    refactor(donations, requests): Simplify components by removing unused imports and code

--- apps/mobile/app/(root)/(drawer)/donations.tsx
+++ apps/mobile/app/(root)/(drawer)/donations.tsx
@@ -1,36 +1,10 @@
-import { BottomSheetActionButton } from '@/components/buttons';
-import { ScrollProvider, useScroll } from '@/components/contexts/ScrollProvider';
 import SafeArea from '@/components/SafeArea';
 import { AvailableDonationsTab } from '@/components/tabs/AvailableDonationsTab';
-import { useRouter } from 'expo-router';
-import { PlusIcon } from 'lucide-react-native';
-
-function ListPage() {
-  const router = useRouter();
-  const { scrollValue } = useScroll();
-
-  function handleCreateNew() {
-    router.push(`/donations/create`);
-  }
-
-  return (
-    <SafeArea safeTop={false} safeBottom={false}>
-      <AvailableDonationsTab />
-      <BottomSheetActionButton
-        icon={PlusIcon}
-        scrollValue={scrollValue}
-        animateDistance={200}
-        label={`Create New Donation`}
-        onPress={handleCreateNew}
-      />
-    </SafeArea>
-  );
-}
 
 export default function DonationsPage() {
   return (
-    <ScrollProvider>
-      <ListPage />
-    </ScrollProvider>
+    <SafeArea safeTop={false} safeBottom={false}>
+      <AvailableDonationsTab />
+    </SafeArea>
   );
 }
--- apps/mobile/app/(root)/(drawer)/requests.tsx
+++ apps/mobile/app/(root)/(drawer)/requests.tsx
@@ -1,36 +1,10 @@
-import { BottomSheetActionButton } from '@/components/buttons';
-import { ScrollProvider, useScroll } from '@/components/contexts/ScrollProvider';
 import SafeArea from '@/components/SafeArea';
 import { AvailableRequestsTab } from '@/components/tabs/AvailableRequestsTab';
-import { useRouter } from 'expo-router';
-import { PlusIcon } from 'lucide-react-native';
-
-function ListPage() {
-  const router = useRouter();
-  const { scrollValue } = useScroll();
-
-  function handleCreateNew() {
-    router.push(`/requests/create`);
-  }
-
-  return (
-    <SafeArea safeTop={false} safeBottom={false}>
-      <AvailableRequestsTab />
-      <BottomSheetActionButton
-        icon={PlusIcon}
-        scrollValue={scrollValue}
-        animateDistance={200}
-        label={`Create New Request`}
-        onPress={handleCreateNew}
-      />
-    </SafeArea>
-  );
-}
 
 export default function RequestsPage() {
   return (
-    <ScrollProvider>
-      <ListPage />
-    </ScrollProvider>
+    <SafeArea safeTop={false} safeBottom={false}>
+      <AvailableRequestsTab />
+    </SafeArea>
   );
 }
