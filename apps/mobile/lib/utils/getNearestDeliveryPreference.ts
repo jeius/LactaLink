0eca0c806c91542e13d1bce64453500f48d2acb9
Julius Pahama <pahamajulius@gmail.com>
2025-08-21 11:12:42 +0800

    feat(getNearestDeliveryPreference): add function to retrieve nearest delivery preference based on coordinates

---
+++ apps/mobile/lib/utils/getNearestDeliveryPreference.ts
@@ -0,0 +1,52 @@
+import { DeliveryPreference } from '@lactalink/types';
+import { extractCollection } from '@lactalink/utilities/extractors';
+import { convertDistance, getDistance } from '@lactalink/utilities/geo-utils';
+import { LatLng } from 'react-native-maps';
+import { useLocationStore } from '../stores/locationStore';
+
+/**
+ * Gets the nearest delivery preference based on provided coordinates.
+ * @param deliveryPreferences - The delivery preferences to be selected.
+ * @param locationCoords - The coordinates to measure the distance from. Defaults to user's current location.
+ * @returns - An object containing the nearest delivery preference and its distance(km), or null if no preferences are available.
+ */
+export function getNearestDeliveryPreference<T extends DeliveryPreference[] | null | undefined>(
+  deliveryPreferences: T,
+  locationCoords?: LatLng | null
+): T extends DeliveryPreference[]
+  ? { deliveryPreference: DeliveryPreference | null; distance: number | null }
+  : null {
+  const currentLocationCoords = useLocationStore.getState().coordinates;
+  const coords = locationCoords === undefined ? currentLocationCoords : locationCoords;
+
+  if (!coords || !deliveryPreferences)
+    return null as T extends DeliveryPreference[]
+      ? { deliveryPreference: null; distance: null }
+      : null;
+
+  let minDistance = Infinity;
+  let preference: DeliveryPreference | null = null;
+
+  for (const pref of deliveryPreferences) {
+    const address = extractCollection(pref.address);
+    if (!address?.coordinates) continue;
+    const [lng, lat] = address.coordinates;
+    const distance = getDistance({ lat: coords.latitude, lng: coords.longitude }, { lat, lng });
+    if (distance < minDistance) {
+      minDistance = distance;
+      preference = pref;
+    }
+  }
+
+  if (!preference)
+    return { deliveryPreference: null, distance: null } as T extends DeliveryPreference[]
+      ? { deliveryPreference: DeliveryPreference | null; distance: number | null }
+      : null;
+
+  return {
+    deliveryPreference: preference,
+    distance: convertDistance(minDistance, 'km'),
+  } as T extends DeliveryPreference[]
+    ? { deliveryPreference: DeliveryPreference | null; distance: number | null }
+    : null;
+}
