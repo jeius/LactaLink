171244c7465a47fbce30d8a16ef8b796bceb2c64
Julius Pahama <pahamajulius@gmail.com>
2025-08-28 08:23:22 +0800

    refactor(animated): remove unused ShimmerSkeleton and AnimatedView components

--- apps/mobile/components/animated/view.tsx
+++
@@ -1,6 +0,0 @@
-import { cssInterop } from 'nativewind';
-import Animated from 'react-native-reanimated';
-
-cssInterop(Animated.View, { className: 'style' });
-
-export const AnimatedView = Animated.View;
