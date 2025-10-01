c65ffd46f747ea892225f2a9a6de2aec5aa298e7
Julius Pahama <pahamajulius@gmail.com>
2025-10-01 17:53:03 +0800

    feat(donation-details): Refactor DonationDetailsPage to enhance UI with new components and improve functionality

--- apps/mobile/app/(root)/donations/[id]/index.tsx
+++ apps/mobile/app/(root)/donations/[id]/index.tsx
@@ -1,10 +1,12 @@
+import { AnimatedPressable } from '@/components/animated/pressable';
 import { AnimatedProgress } from '@/components/animated/progress';
 import { useTheme } from '@/components/AppProvider/ThemeProvider';
-import { DeliveryPreferenceListCard } from '@/components/cards/DeliveryPreferenceListCard';
+import { DeliveryPreferenceCard } from '@/components/cards/DeliveryPreferenceCard';
 import { MilkBagCard } from '@/components/cards/MilkBagCard';
 import { SingleImageViewer } from '@/components/ImageViewer';
 import { ProfileTag } from '@/components/ProfileTag';
 import SafeArea from '@/components/SafeArea';
+import { CollectionMethodTag, StorageTypeTag } from '@/components/tags';
 import { Box } from '@/components/ui/box';
 import { Button, ButtonIcon } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
@@ -12,21 +14,30 @@ import { Divider } from '@/components/ui/divider';
 import GradientBackground from '@/components/ui/gradient-bg';
 import { HStack } from '@/components/ui/hstack';
 import { Icon, ThreeDotsIcon } from '@/components/ui/icon';
+import { Popover, PopoverBackdrop, PopoverContent } from '@/components/ui/popover';
+import { Pressable } from '@/components/ui/pressable';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Text } from '@/components/ui/text';
+import { Textarea, TextareaInput } from '@/components/ui/textarea';
 import { VStack } from '@/components/ui/vstack';
 import { useMeUser } from '@/hooks/auth/useAuth';
 import { useFetchById } from '@/hooks/collections/useFetchById';
 import { getDonationRequestStatusColor } from '@/lib/colors';
 import { DEVICE_BREAKPOINTS } from '@/lib/constants';
-import { COLLECTION_MODES, DONATION_REQUEST_STATUS, STORAGE_TYPES } from '@lactalink/enums';
-import { DeliveryPreference, Donation, MilkBag } from '@lactalink/types/payload-generated-types';
+import { DONATION_REQUEST_STATUS } from '@lactalink/enums';
+import { DeliveryPreference, MilkBag } from '@lactalink/types/payload-generated-types';
 import { generatePlaceHoldersWithID } from '@lactalink/utilities';
 import { isPlaceHolderData } from '@lactalink/utilities/checkers';
 import { extractCollection, extractOneImageData } from '@lactalink/utilities/extractors';
 import { formatDate, formatLocaleTime } from '@lactalink/utilities/formatters';
-import { useLocalSearchParams } from 'expo-router';
-import { DropletIcon, MessageCircleIcon, PackageIcon, ShareIcon } from 'lucide-react-native';
+import { Link, useLocalSearchParams } from 'expo-router';
+import {
+  EditIcon,
+  MessageCircleIcon,
+  ShareIcon,
+  Trash2Icon,
+  XCircleIcon,
+} from 'lucide-react-native';
 import React, { ComponentProps, useMemo } from 'react';
 import { useWindowDimensions } from 'react-native';
 import { FlatList, ScrollView } from 'react-native-gesture-handler';
@@ -54,6 +65,7 @@ export default function DonationDetailsPage() {
   const isOwner = meUserProfile?.id === donor?.id;
 
   const status = data?.status;
+  const notes = data?.details?.notes;
 
   const { image, bags } = useMemo(() => {
     const milkSample = extractCollection(data?.details?.milkSample);
@@ -102,20 +114,9 @@ export default function DonationDetailsPage() {
             style={{ opacity: 0.6 }}
           />
 
-          <HStack
-            space="md"
-            className="absolute inset-x-0 bottom-0 -mb-5 items-center justify-end px-5"
-          >
-            <Button action="muted" className="h-fit w-fit rounded-full p-3">
-              <ButtonIcon as={MessageCircleIcon} />
-            </Button>
-            <Button action="muted" className="h-fit w-fit rounded-full p-3">
-              <ButtonIcon as={ShareIcon} />
-            </Button>
-            <Button action="muted" className="h-fit w-fit rounded-full p-3">
-              <ButtonIcon as={ThreeDotsIcon} />
-            </Button>
-          </HStack>
+          <Box className="absolute inset-x-0 bottom-0 -mb-5 px-5">
+            <CTA isOwner={!!isOwner} status={status} />
+          </Box>
         </Box>
 
         <HStack className="mt-4 items-center justify-between p-5">
@@ -156,7 +157,7 @@ export default function DonationDetailsPage() {
             <>
               <AnimatedProgress size="sm" orientation="horizontal" value={percentage} />
               <Text size="xs" className="text-typography-700 text-center">
-                {remainingVolume} mL remaining
+                {remainingVolume} mL available
               </Text>
             </>
           )}
@@ -164,7 +165,26 @@ export default function DonationDetailsPage() {
 
         <Divider />
 
-        <Tags isLoading={isLoading} data={data} />
+        <HStack space="2xl" className="flex-wrap items-center p-5">
+          <StorageTypeTag isLoading={isLoading} data={data} />
+          <CollectionMethodTag isLoading={isLoading} data={data} />
+        </HStack>
+
+        <VStack className="mb-5 px-5">
+          <Text className="font-JakartaSemiBold mb-1">Notes</Text>
+          {isLoading ? (
+            <Skeleton className="h-20" />
+          ) : (
+            <Textarea size="sm" pointerEvents="none">
+              <TextareaInput
+                defaultValue={notes || ''}
+                placeholder="No notes provided."
+                editable={false}
+                style={{ textAlignVertical: 'top' }}
+              />
+            </Textarea>
+          )}
+        </VStack>
 
         <MilkBagList isLoading={isLoading} data={extractCollection(bags) || []} className="mb-5" />
 
@@ -182,47 +202,13 @@ export default function DonationDetailsPage() {
 
 const PLACEHOLDER_DATA = generatePlaceHoldersWithID(4, {});
 
-interface TagsProps {
-  isLoading?: boolean;
-  data?: Donation;
-}
-function Tags({ isLoading, data }: TagsProps) {
-  const storage = STORAGE_TYPES[data?.details.storageType || 'OTHER'].label;
-  const method = COLLECTION_MODES[data?.details.collectionMode || 'MANUAL'].label;
-  return (
-    <HStack space="2xl" className="flex-wrap p-5">
-      {isLoading ? (
-        <>
-          <Skeleton variant="circular" className="h-7 w-32" />
-          <Skeleton variant="circular" className="h-7 w-32" />
-        </>
-      ) : (
-        <>
-          <HStack space="sm" className="items-center">
-            <Card className="rounded-full border-0 p-2">
-              <Icon as={PackageIcon} size="sm" />
-            </Card>
-            <Text size="sm">{storage}</Text>
-          </HStack>
-          <HStack space="sm" className="items-center">
-            <Card className="rounded-full border-0 p-2">
-              <Icon as={DropletIcon} size="sm" />
-            </Card>
-            <Text size="sm">{method}</Text>
-          </HStack>
-        </>
-      )}
-    </HStack>
-  );
-}
-
 interface PreferenceListProps extends ComponentProps<typeof VStack> {
   data: DeliveryPreference[];
   isLoading?: boolean;
 }
 
 function PreferenceList({ data, isLoading, ...props }: PreferenceListProps) {
-  if (data.length === 0) {
+  if (!isLoading && data.length === 0) {
     return <Text size="sm">No delivery preferences specified.</Text>;
   }
 
@@ -238,7 +224,17 @@ function PreferenceList({ data, isLoading, ...props }: PreferenceListProps) {
         contentContainerClassName="px-5"
         renderItem={({ item }) => {
           const isLoading = isPlaceHolderData(item);
-          return <DeliveryPreferenceListCard data={item} isLoading={isLoading} />;
+          return (
+            <Link asChild push href={`/delivery-preferences/${item.id}`}>
+              <AnimatedPressable className="overflow-hidden rounded-2xl">
+                <DeliveryPreferenceCard
+                  appearance="compact"
+                  preference={item}
+                  isLoading={isLoading}
+                />
+              </AnimatedPressable>
+            </Link>
+          );
         }}
       />
     </VStack>
@@ -251,7 +247,7 @@ interface MilkBagListProps extends ComponentProps<typeof VStack> {
 }
 
 function MilkBagList({ data, isLoading, ...props }: MilkBagListProps) {
-  if (data.length === 0) {
+  if (!isLoading && data.length === 0) {
     return <Text size="sm">No milk bags available.</Text>;
   }
 
@@ -277,13 +273,73 @@ function MilkBagList({ data, isLoading, ...props }: MilkBagListProps) {
 interface CTAProps {
   isOwner: boolean;
   status?: keyof typeof DONATION_REQUEST_STATUS;
-  onPress(): () => void;
 }
-function CTA({ isOwner, status, onPress }: CTAProps) {
-  let label = 'Request This Donation';
+function CTA({ isOwner, status }: CTAProps) {
+  const isEditable =
+    status &&
+    (
+      [DONATION_REQUEST_STATUS.PENDING.value, DONATION_REQUEST_STATUS.AVAILABLE.value] as string[]
+    ).includes(status);
 
-  if (isOwner) {
-  }
+  const isDeletable =
+    status &&
+    (
+      [DONATION_REQUEST_STATUS.EXPIRED.value, DONATION_REQUEST_STATUS.CANCELLED.value] as string[]
+    ).includes(status);
+
+  return (
+    <HStack space="md" className="items-center justify-end">
+      <Button action="muted" className="h-fit w-fit rounded-full p-3">
+        <ButtonIcon as={MessageCircleIcon} />
+      </Button>
+      <Button action="muted" className="h-fit w-fit rounded-full p-3">
+        <ButtonIcon as={ShareIcon} />
+      </Button>
+
+      <Popover
+        placement="top left"
+        offset={2}
+        trigger={(props) => (
+          <Button {...props} action="muted" className="h-fit w-fit rounded-full p-3">
+            <ButtonIcon as={ThreeDotsIcon} />
+          </Button>
+        )}
+      >
+        <PopoverBackdrop />
+        <PopoverContent className="flex-col p-2">
+          {isOwner ? (
+            <>
+              {isEditable && (
+                <Pressable className="flex-row items-center overflow-hidden rounded-md p-2">
+                  <Icon as={EditIcon} size="sm" className="mr-2" />
+                  <Text size="sm">Edit</Text>
+                </Pressable>
+              )}
+              {isDeletable && (
+                <Pressable className="flex-row items-center overflow-hidden rounded-md p-2">
+                  <Icon as={Trash2Icon} size="sm" className="text-error-500 mr-2" />
+                  <Text size="sm" className="text-error-500">
+                    Delete
+                  </Text>
+                </Pressable>
+              )}
+            </>
+          ) : (
+            <>
+              {isEditable && (
+                <Pressable className="flex-row items-center overflow-hidden rounded-md p-2">
+                  <Icon as={XCircleIcon} size="sm" className="text-error-500 mr-2" />
+                  <Text size="sm" className="text-error-500">
+                    Cancel
+                  </Text>
+                </Pressable>
+              )}
+            </>
+          )}
+        </PopoverContent>
+      </Popover>
+    </HStack>
+  );
 }
 
 // #endregion Components
