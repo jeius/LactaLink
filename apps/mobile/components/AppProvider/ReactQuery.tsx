450dadac68781a9eee8afca303f0d411695d206d
Julius Pahama <pahamajulius@gmail.com>
2025-11-13 09:45:01 +0800

    feat(react-query): Add global mutation config. Enhance mutation handling with success and error messages

--- apps/mobile/components/AppProvider/ReactQuery.tsx
+++ apps/mobile/components/AppProvider/ReactQuery.tsx
@@ -1,8 +1,39 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MutationCache, QueryClient, QueryClientProvider, QueryKey } from '@tanstack/react-query';
 
 import React, { ReactNode } from 'react';
+import { toast } from 'sonner-native';
 
-const queryClient = new QueryClient();
+declare module '@tanstack/react-query' {
+  interface Register {
+    mutationMeta: {
+      successMessage?: string;
+      errorMessage?: string;
+      invalidatesQuery?: QueryKey;
+    };
+  }
+}
+
+const queryClient = new QueryClient({
+  mutationCache: new MutationCache({
+    onError: (_error, _vars, _context, mutation) => {
+      if (mutation.meta?.errorMessage) {
+        toast.error(mutation.meta.errorMessage);
+      }
+    },
+
+    onSuccess: (_data, _vars, _context, mutation) => {
+      if (mutation.meta?.invalidatesQuery) {
+        queryClient.invalidateQueries({
+          queryKey: mutation.meta.invalidatesQuery,
+        });
+      }
+
+      if (mutation.meta?.successMessage) {
+        toast.success(mutation.meta.successMessage);
+      }
+    },
+  }),
+});
 
 export function ReactQuery({ children }: { children: ReactNode }) {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
