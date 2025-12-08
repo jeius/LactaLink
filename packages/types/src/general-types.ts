da03e6c32179c6eb21d808d35e3a8b7f53f37bb5
Julius Pahama <pahamajulius@gmail.com>
2025-12-09 00:23:50 +0800

    feat(utilities): Add age calculation and formatting functions

--- packages/types/src/general-types.ts
+++ packages/types/src/general-types.ts
@@ -1,5 +1,14 @@
 import { DAYS, DELIVERY_OPTIONS, DONATION_REQUEST_STATUS } from '@lactalink/enums';
-import { Identity, Individual, Post, Transaction, User } from './payload-types/generated';
+import { CollectionSlug } from './payload-types';
+import {
+  Hospital,
+  Identity,
+  Individual,
+  MilkBank,
+  Post,
+  Transaction,
+  User,
+} from './payload-types/generated';
 
 export type Theme = 'light' | 'dark';
 export type ImageData = {
@@ -21,3 +30,17 @@ export type TransactionStatus = Transaction['status'];
 export type MediaAttachment = NonNullable<Post['attachments']>[number];
 
 export type UserProfile = NonNullable<User['profile']>;
+
+export type PopulatedUserProfile =
+  | {
+      relationTo: Extract<CollectionSlug, 'individuals'>;
+      value: Individual;
+    }
+  | {
+      relationTo: Extract<CollectionSlug, 'hospitals'>;
+      value: Hospital;
+    }
+  | {
+      relationTo: Extract<CollectionSlug, 'milkBanks'>;
+      value: MilkBank;
+    };
