3ab91584b267ac59289b4c0dfb303591327e1a63
Julius Pahama <pahamajulius@gmail.com>
2026-01-28 21:25:11 +0800

    refactor(frontend-delivery-proposal): Move component from DeliveryProposalScreen into component/screens directory. Refactor its Stack.Screen presentation to formsheet.

--- apps/mobile/app/(root)/transactions/_layout.tsx
+++ apps/mobile/app/(root)/transactions/_layout.tsx
@@ -1,17 +1,29 @@
+import { useTheme } from '@/components/AppProvider/ThemeProvider';
 import { useScreenOptions } from '@/hooks/useScreenOptions';
 import { Stack } from 'expo-router';
 import React from 'react';
+import { useWindowDimensions } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function TransactionsLayout() {
   const screenOptions = useScreenOptions();
+  const { themeColors } = useTheme();
+
+  const inset = useSafeAreaInsets();
+  const screen = useWindowDimensions();
+
+  const allowedDetend = (screen.height - inset.top) / screen.height;
   return (
     <Stack screenOptions={screenOptions}>
       <Stack.Screen
         name="propose"
         options={{
-          presentation: 'transparentModal',
-          contentStyle: { backgroundColor: 'transparent' },
+          presentation: 'formSheet',
+          contentStyle: { backgroundColor: themeColors.background[0] },
           animation: 'slide_from_bottom',
+          sheetElevation: 24,
+          sheetCornerRadius: 32,
+          sheetAllowedDetents: [allowedDetend],
         }}
       />
     </Stack>
