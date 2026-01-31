866f4840b95b33b83921b8f632c9a46862bb10f8
Julius Pahama <pahamajulius@gmail.com>
2026-02-01 04:26:16 +0800

    feat(refresh-control): enhance RefreshControl to manage refreshing state and improve functionality

--- apps/mobile/components/RefreshControl.tsx
+++ apps/mobile/components/RefreshControl.tsx
@@ -1,15 +1,21 @@
 import { useRevalidateAllQueries } from '@/hooks/collections/useRevalidateQueries';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { RefreshControlProps } from 'react-native';
 import { RefreshControl as RNRefreshControl } from 'react-native-gesture-handler';
 import { useTheme } from './AppProvider/ThemeProvider';
 
-const RefreshControl: FC<RefreshControlProps> = ({ onRefresh, ...props }) => {
+const RefreshControl: FC<RefreshControlProps> = ({ onRefresh, refreshing, ...props }) => {
   const { themeColors } = useTheme();
   const revalidateQueries = useRevalidateAllQueries();
 
+  const [pulled, setPulled] = useState(false);
+
   const tintColor = themeColors.primary[500];
   const bgColor = themeColors.background[50];
+
+  useEffect(() => {
+    if (!refreshing) setPulled(false);
+  }, [refreshing]);
 
   function handleRefresh() {
     if (onRefresh) {
@@ -17,11 +23,13 @@ const RefreshControl: FC<RefreshControlProps> = ({ onRefresh, ...props }) => {
     } else {
       revalidateQueries();
     }
+    setPulled(true);
   }
 
   return (
     <RNRefreshControl
       {...props}
+      refreshing={pulled && refreshing}
       colors={tintColor ? [tintColor] : undefined}
       progressBackgroundColor={bgColor}
       onRefresh={handleRefresh}
