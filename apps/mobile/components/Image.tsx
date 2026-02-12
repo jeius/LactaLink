222cd59a7645e6ae9ae239e46407102355b1289e
Julius Pahama <pahamajulius@gmail.com>
2026-02-12 22:09:49 +0800

    feat(Image): refactor Image component to use cssInterop for styling

--- apps/mobile/components/Image.tsx
+++ apps/mobile/components/Image.tsx
@@ -1,7 +1,12 @@
 import { appendHeaders } from '@/lib/utils/appendHeaders';
 import { Image as ExpoImage, type ImageProps } from 'expo-image';
+import { cssInterop } from 'nativewind';
 
-export function Image(props: ImageProps) {
+function Image(props: ImageProps) {
   const transformedSource = appendHeaders(props);
   return <ExpoImage {...props} source={transformedSource} />;
 }
+
+const StyledImage = cssInterop(Image, { className: 'style' });
+
+export { StyledImage as Image };
