6a9c055240ccf4ea271fc70024c6e1b6d844648c
Julius Pahama <pahamajulius@gmail.com>
2026-02-18 01:29:03 +0800

    feat(sheet): enhance event handling with focus and dismiss callbacks

--- apps/mobile/components/ui/sheet/Sheet.tsx
+++ apps/mobile/components/ui/sheet/Sheet.tsx
@@ -1,10 +1,19 @@
 import { useTheme } from '@/components/AppProvider/ThemeProvider';
-import { TrueSheet, TrueSheetProps } from '@lodev09/react-native-true-sheet';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {
+  DidBlurEvent,
+  DidDismissEvent,
+  DidFocusEvent,
+  DidPresentEvent,
+  TrueSheet,
+  TrueSheetProps,
+} from '@lodev09/react-native-true-sheet';
+import { useFocusEffect } from 'expo-router';
+import { cssInterop } from 'nativewind';
+import React, { useCallback, useRef } from 'react';
+import { BackHandler, StyleSheet, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-export type SheetRef = React.ComponentRef<typeof TrueSheet>;
+export type SheetRef = TrueSheet;
 
 export type SheetProps = TrueSheetProps;
 
@@ -15,6 +24,47 @@ const Sheet = React.forwardRef<SheetRef, SheetProps>(function Sheet(props, ref) 
   const backgroundColor = themeColors.background[0];
   const primaryColor = themeColors.primary[500];
 
+  const presentedRef = useRef(false);
+  const focusRef = useRef(false);
+
+  const handleOnDidDismiss = (e: DidDismissEvent) => {
+    props.onDidDismiss?.(e);
+    if (e.defaultPrevented) return;
+    presentedRef.current = false;
+  };
+
+  const handleOnDidPresent = (e: DidPresentEvent) => {
+    props.onDidPresent?.(e);
+    if (e.defaultPrevented) return;
+    presentedRef.current = true;
+  };
+
+  const handleOnDidFocus = (e: DidFocusEvent) => {
+    props.onDidFocus?.(e);
+    if (e.defaultPrevented) return;
+    focusRef.current = true;
+  };
+
+  const handleOnDidBlur = (e: DidBlurEvent) => {
+    props.onDidBlur?.(e);
+    if (e.defaultPrevented) return;
+    focusRef.current = false;
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      const subscription = BackHandler.addEventListener('hardwareBackPress', () => {
+        if (focusRef.current && presentedRef.current) {
+          if (typeof ref === 'object' && ref) ref.current?.dismiss();
+          return true;
+        }
+        return false;
+      });
+
+      return () => subscription.remove();
+    }, [ref])
+  );
+
   return (
     <TrueSheet
       {...props}
@@ -24,6 +74,10 @@ const Sheet = React.forwardRef<SheetRef, SheetProps>(function Sheet(props, ref) 
       cornerRadius={props.cornerRadius ?? 24}
       header={props.header ?? <View />}
       headerStyle={StyleSheet.flatten([{ marginTop: 30 }, props.headerStyle])}
+      onDidDismiss={handleOnDidDismiss}
+      onDidPresent={handleOnDidPresent}
+      onDidFocus={handleOnDidFocus}
+      onDidBlur={handleOnDidBlur}
       grabberOptions={{
         color: primaryColor,
         adaptive: false,
@@ -32,5 +86,7 @@ const Sheet = React.forwardRef<SheetRef, SheetProps>(function Sheet(props, ref) 
     />
   );
 });
+
+cssInterop(Sheet, { className: 'style' });
 
 export default Sheet;
