120911db682fd22198ebb2bd5c462f9020a1177b
Julius Pahama <pahamajulius@gmail.com>
2026-03-02 12:05:00 +0800

    fix(usePreventBackPress): allow RefObject for prevent parameter to enhance back press handling

--- apps/mobile/hooks/usePreventBackPress.ts
+++ apps/mobile/hooks/usePreventBackPress.ts
@@ -1,12 +1,12 @@
 import { useFocusEffect } from 'expo-router';
-import { useCallback } from 'react';
+import { RefObject, useCallback } from 'react';
 import { BackHandler } from 'react-native';
 
-export function usePreventBackPress(prevent: boolean, callBack?: () => void) {
+export function usePreventBackPress(prevent: boolean | RefObject<boolean>, callBack?: () => void) {
   useFocusEffect(
     useCallback(() => {
       const onBackPress = () => {
-        if (prevent) {
+        if (typeof prevent === 'boolean' ? prevent : prevent.current) {
           callBack?.();
           return true;
         }
