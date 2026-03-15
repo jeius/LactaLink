38c525fc82255d56d9c3553a4f6dbfb0bd5432ee
Julius Pahama <pahamajulius@gmail.com>
2026-03-15 15:10:04 +0800

    feat(profile-mutations): create profile update and creation mutations with improved error handling and signal support

--- apps/mobile/features/profile/hooks/mutations.ts
+++ apps/mobile/features/profile/hooks/mutations.ts
@@ -1,7 +1,25 @@
 import { PopulatedUserProfile } from '@lactalink/types';
 import { useMutation } from '@tanstack/react-query';
-import { createUpdateProfileMutation } from '../lib/mutationOptions';
+import { useMemo } from 'react';
+import {
+  createEditProfileMutationOptions,
+  createNewProfileMutationOptions,
+} from '../lib/mutationOptions';
 
 export function useUpdateProfileMutation(profile: PopulatedUserProfile) {
-  return useMutation(createUpdateProfileMutation(profile));
+  const controller = useMemo(() => new AbortController(), []);
+
+  const mutation = useMutation(
+    createEditProfileMutationOptions(profile, { signal: controller.signal })
+  );
+
+  return { ...mutation, cancelMutate: () => controller.abort() };
+}
+
+export function useCreateProfileMutation() {
+  const controller = useMemo(() => new AbortController(), []);
+
+  const mutation = useMutation(createNewProfileMutationOptions({ signal: controller.signal }));
+
+  return { ...mutation, cancelMutate: () => controller.abort() };
 }
--- apps/mobile/features/profile/lib/mutationOptions.ts
+++ apps/mobile/features/profile/lib/mutationOptions.ts
@@ -1,32 +1,84 @@
-import { uploadImage } from '@/lib/api/file';
+import { QUERY_KEYS } from '@/lib/constants';
 import { getApiClient } from '@lactalink/api';
 import { SetupProfileSchema } from '@lactalink/form-schemas';
 import { PopulatedUserProfile } from '@lactalink/types';
+import { Hospital, Individual, MilkBank } from '@lactalink/types/payload-generated-types';
+import { CollectionSlug } from '@lactalink/types/payload-types';
+import { extractID } from '@lactalink/utilities/extractors';
 import { mutationOptions } from '@tanstack/react-query';
+import { File } from 'expo-file-system';
 import { createUserProfileQuery } from './queryOption';
 
-export function createUpdateProfileMutation(profile: PopulatedUserProfile) {
+const slugMap: Record<
+  SetupProfileSchema['profileType'],
+  Extract<CollectionSlug, 'hospitals' | 'individuals' | 'milkBanks'>
+> = {
+  HOSPITAL: 'hospitals',
+  INDIVIDUAL: 'individuals',
+  MILK_BANK: 'milkBanks',
+};
+
+export function createEditProfileMutationOptions(
+  profile: PopulatedUserProfile,
+  init?: RequestInit
+) {
   return mutationOptions({
     mutationKey: ['profile', 'update'],
     mutationFn: async ({ avatar, profileType: _, ...data }: Partial<SetupProfileSchema>) => {
-      const avatarDoc = avatar && (await uploadImage('avatars', avatar));
+      const avatarURI = avatar?.url;
 
-      return getApiClient().updateByID({
-        collection: profile.relationTo,
-        id: profile.value.id,
-        data: {
-          ...data,
-          avatar: avatarDoc === null ? null : avatarDoc?.id,
-        },
-        depth: 3,
+      const response = await getApiClient().request({
+        path: `/${profile.relationTo}/${extractID(profile.value)}`,
+        method: 'PATCH',
+        file: avatarURI ? new File(avatarURI) : undefined,
+        json: data,
+        init: init,
+        args: { depth: 3 },
       });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Failed to update profile');
+      }
+
+      const updatedProfile: Individual | Hospital | MilkBank = await response.json();
+      return { ...profile, value: updatedProfile } as PopulatedUserProfile;
     },
     onSuccess: (data, _vars, _ctx, { client }) => {
       const queryKey = createUserProfileQuery(profile).queryKey;
-      client.setQueryData(queryKey, {
-        relationTo: profile.relationTo,
-        value: data,
-      } as PopulatedUserProfile);
+      client.setQueryData(queryKey, data);
+    },
+  });
+}
+
+export function createNewProfileMutationOptions(init?: RequestInit) {
+  return mutationOptions({
+    mutationKey: ['profile', 'create'],
+    mutationFn: async ({ avatar, profileType, ...data }: SetupProfileSchema) => {
+      const avatarURI = avatar?.url;
+
+      const response = await getApiClient().request({
+        path: `/${slugMap[profileType]}`,
+        method: 'POST',
+        file: avatarURI ? new File(avatarURI) : undefined,
+        json: data,
+        init: init,
+        args: { depth: 3 },
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Failed to create profile');
+      }
+
+      const profile: Individual | Hospital | MilkBank = await response.json();
+      return { relationTo: slugMap[profileType], value: profile } as PopulatedUserProfile;
+    },
+    onSuccess: async (data, _vars, _ctx, { client }) => {
+      const queryKey = createUserProfileQuery(data).queryKey;
+      client.setQueryData(queryKey, data);
+
+      await client.invalidateQueries({ queryKey: QUERY_KEYS.AUTH.SESSION });
     },
   });
 }
