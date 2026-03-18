1cadcf947cfcd71bbe7b63a3c0cbd1c89cc0174f
Julius Pahama <pahamajulius@gmail.com>
2026-03-18 11:37:44 +0800

    feat(eslint-config): add custom rules for unused variable handling in ESLint configuration

--- packages/eslint-config/expo.js
+++ packages/eslint-config/expo.js
@@ -5,4 +5,24 @@ import base from './base.js';
  * A custom ESLint configuration for libraries that use React Native Expo.
  *
  */
-export default [...base, ...expo];
+export default [
+  ...base,
+  ...expo,
+  {
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: false,
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^(_|ignore)',
+        },
+      ],
+    },
+  },
+];
