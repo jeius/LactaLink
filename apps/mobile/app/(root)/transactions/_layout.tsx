1f31923c3d39ac2e9951a90b2029b56f9fc5a5a7
Julius Pahama <pahamajulius@gmail.com>
2026-03-24 12:08:52 +0800

    refactor(TransactionsLayout): simplify screen options and remove unused imports

--- apps/mobile/app/(root)/transactions/_layout.tsx
+++ apps/mobile/app/(root)/transactions/_layout.tsx
@@ -1,31 +1,13 @@
-import { useTheme } from '@/components/AppProvider/ThemeProvider';
-import { useScreenOptions } from '@/hooks/useScreenOptions';
+import { useScreenFormSheetOptions, useScreenOptions } from '@/hooks/useScreenOptions';
 import { Stack } from 'expo-router';
 import React from 'react';
-import { useWindowDimensions } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function TransactionsLayout() {
   const screenOptions = useScreenOptions();
-  const { themeColors } = useTheme();
-
-  const inset = useSafeAreaInsets();
-  const screen = useWindowDimensions();
-
-  const allowedDetend = (screen.height - inset.top) / screen.height;
+  const formSheetOptions = useScreenFormSheetOptions();
   return (
     <Stack screenOptions={screenOptions}>
-      <Stack.Screen
-        name="propose"
-        options={{
-          presentation: 'formSheet',
-          contentStyle: { backgroundColor: themeColors.background[0] },
-          animation: 'slide_from_bottom',
-          sheetElevation: 24,
-          sheetCornerRadius: 32,
-          sheetAllowedDetents: [allowedDetend],
-        }}
-      />
+      <Stack.Screen name="propose" options={formSheetOptions} />
     </Stack>
   );
 }
