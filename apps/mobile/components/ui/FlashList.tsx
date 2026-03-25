7ad67c644e8cd01293324f2880941df72e58d351
Julius Pahama <pahamajulius@gmail.com>
2026-03-26 00:39:47 +0800

    feat(FlashList): enhance refresh control handling and improve type definitions

--- apps/mobile/components/ui/FlashList.tsx
+++ apps/mobile/components/ui/FlashList.tsx
@@ -1,13 +1,27 @@
 import { FlashList as FlashListComponent, FlashListProps, FlashListRef } from '@shopify/flash-list';
 import { cssInterop } from 'nativewind';
-import React, { ForwardedRef, forwardRef } from 'react';
+import { forwardRef, type FC, type ForwardedRef, type PropsWithoutRef } from 'react';
 import { Platform, ViewProps } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { RefreshControl } from '../RefreshControl';
 
-type Props<T> = Omit<FlashListProps<T>, 'refreshControl'>;
+type Props<T> = Omit<FlashListProps<T>, 'refreshControl'> & {
+  headerClassName?: ViewProps['className'];
+  footerClassName?: ViewProps['className'];
+};
 
-function FlashListInner<T>(props: Props<T>, ref: ForwardedRef<FlashListRef<T>>) {
+/**
+ * A wrapper around the FlashList component from `@shopify/flash-list` that provides some
+ * default props and handles the refresh control.
+ *
+ * @remarks
+ * Android: Disables the pull-to-refresh functionality when `nestedScrollEnabled` is
+ * `true` due to Android limitations.
+ */
+const FlashList = forwardRef(function FlashList<T>(
+  props: Props<T>,
+  ref: ForwardedRef<FlashListRef<T>>
+) {
   const {
     showsHorizontalScrollIndicator = false,
     showsVerticalScrollIndicator = false,
@@ -27,51 +41,41 @@ function FlashListInner<T>(props: Props<T>, ref: ForwardedRef<FlashListRef<T>>) 
       showsVerticalScrollIndicator={showsVerticalScrollIndicator}
       nestedScrollEnabled={nestedScrollEnabled}
       renderScrollComponent={renderScrollComponent}
-      refreshControl={Platform.select({
-        // Note: nestedScrollEnabled on Android causes the FlashList to ignore the
-        // built-in RefreshControl, so we conditionally render it based on that prop.
-        android: nestedScrollEnabled ? undefined : (
-          <RefreshControl
-            progressViewOffset={progressViewOffset}
-            refreshing={refreshing ?? false}
-            onRefresh={onRefresh ?? (() => {})}
-          />
-        ),
-        default: (
-          <RefreshControl
-            progressViewOffset={progressViewOffset}
-            refreshing={refreshing ?? false}
-            onRefresh={onRefresh ?? (() => {})}
-          />
-        ),
-      })}
+      refreshControl={
+        !onRefresh
+          ? undefined
+          : Platform.select({
+              // Note: nestedScrollEnabled on Android causes the FlashList to ignore the
+              // built-in RefreshControl, so we conditionally render it based on that prop.
+              android: nestedScrollEnabled ? undefined : (
+                <RefreshControl
+                  progressViewOffset={progressViewOffset}
+                  refreshing={refreshing ?? false}
+                  onRefresh={onRefresh}
+                />
+              ),
+              default: (
+                <RefreshControl
+                  progressViewOffset={progressViewOffset}
+                  refreshing={refreshing ?? false}
+                  onRefresh={onRefresh}
+                />
+              ),
+            })
+      }
     />
   );
-}
-
-/**
- * A wrapper around the FlashList component from `@shopify/flash-list` that provides some
- * default props and handles the refresh control.
- *
- * @remarks
- * Android: Disables the pull-to-refresh functionality when `nestedScrollEnabled` is
- * `true` due to Android limitations.
- */
-const FlashList = forwardRef(FlashListInner) as <T>(
-  props: Props<T> & React.RefAttributes<FlashListRef<T>>
+}) as <T>(
+  props: PropsWithoutRef<Props<T>> & React.RefAttributes<FlashListRef<T>>
 ) => React.ReactElement;
 
-const StyledFlashList = cssInterop(FlashList, {
+(FlashList as FC).displayName = 'FlashList';
+
+cssInterop(FlashList, {
   headerClassName: 'ListHeaderComponentStyle',
   footerClassName: 'ListFooterComponentStyle',
-}) as <T>(
-  props: Props<T> &
-    React.RefAttributes<FlashListRef<T>> & {
-      headerClassName?: ViewProps['className'];
-      footerClassName?: ViewProps['className'];
-    }
-) => React.ReactElement;
+});
 
 export type { FlashListRef, ListRenderItem, ListRenderItemInfo } from '@shopify/flash-list';
-export { StyledFlashList as FlashList };
+export { FlashList };
 export type { Props as FlashListProps };
