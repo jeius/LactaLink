bfecfca0b785692d4e2ceb1647046409cd067a27
Julius Pahama <pahamajulius@gmail.com>
2026-04-15 14:12:49 +0800

    feat(blocks): add width options for layout customization

--- packages/enums/src/blocks.ts
+++ packages/enums/src/blocks.ts
@@ -12,3 +12,12 @@ export const CHOICE_TYPE = {
   PREDEFINED: { label: 'Predefined', value: 'PREDEFINED' },
   CUSTOM: { label: 'User Defined', value: 'CUSTOM' },
 } as const;
+
+export const WIDTH_OPTIONS = {
+  full: { value: 'full', label: '100%' },
+  '3/4': { value: '3/4', label: '75%' },
+  '2/3': { value: '2/3', label: '66%' },
+  '1/2': { value: '1/2', label: '50%' },
+  '1/3': { value: '1/3', label: '33%' },
+  '1/4': { value: '1/4', label: '25%' },
+} as const;
