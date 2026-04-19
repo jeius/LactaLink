569aa63994a6df2636e162be973eaae0bd55e5b1
Julius Pahama <pahamajulius@gmail.com>
2026-04-19 22:27:45 +0800

    fix(FormSheetHandle): add pointer-events-none class to prevent interaction

--- apps/mobile/components/ui/FormSheetHandle.tsx
+++ apps/mobile/components/ui/FormSheetHandle.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { View } from 'react-native';
 
 export default function FormSheetHandle() {
   return (
-    <View className="h-10 items-center justify-center">
+    <View className="pointer-events-none h-10 items-center justify-center">
       <View className="h-1 w-12 rounded-full bg-primary-500" />
     </View>
   );
