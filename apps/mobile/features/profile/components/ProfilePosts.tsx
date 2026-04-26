812334b15fffa2915525a0523c622e8b6cf58bad
Julius Pahama <pahamajulius@gmail.com>
2026-04-26 10:47:49 +0800

    refactor(profile-posts): streamline post rendering with InfiniteFlashList and enhance empty state handling

--- apps/mobile/features/profile/components/ProfilePosts.tsx
+++ apps/mobile/features/profile/components/ProfilePosts.tsx
@@ -1,7 +1,5 @@
-import { RefreshControl } from '@/components/RefreshControl';
-import { Box } from '@/components/ui/box';
 import { Button, ButtonText } from '@/components/ui/button';
-import { Spinner } from '@/components/ui/spinner';
+import { InfiniteFlashList } from '@/components/ui/list';
 import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
 import PostItem from '@/features/feed/components/post-item/PostItem';
@@ -10,11 +8,10 @@ import { useInfiniteUserPosts } from '@/features/profile/hooks/queries';
 import { getMeUser } from '@/lib/stores/meUserStore';
 import { PopulatedUserProfile } from '@lactalink/types';
 import { Post } from '@lactalink/types/payload-generated-types';
-import { isEqualProfiles, isPlaceHolderData } from '@lactalink/utilities/checkers';
+import { isEqualProfiles } from '@lactalink/utilities/checkers';
 import { listKeyExtractor } from '@lactalink/utilities/extractors';
-import { FlashList, ListRenderItem } from '@shopify/flash-list';
 import { Link, useRouter } from 'expo-router';
-import { useCallback } from 'react';
+import { PropsWithChildren, useCallback } from 'react';
 
 export interface PostListProps<T extends PopulatedUserProfile> {
   profile: T;
@@ -29,66 +26,70 @@ export default function ProfilePosts<T extends PopulatedUserProfile>({
   onRefresh,
   HeaderComponent,
 }: PostListProps<T>) {
-  const router = useRouter();
   const meUser = getMeUser();
   const isMeUser = isEqualProfiles(meUser?.profile, profile);
 
   const { data: posts, ...postsQuery } = useInfiniteUserPosts(profile);
-
-  const renderItem: ListRenderItem<Post> = useCallback(
-    ({ item }) => {
-      if (isPlaceHolderData(item)) return <PostPlaceholderItem />;
-
-      const handlePress = () => router.push(`/feed/${item.id}`);
-      return <PostItem post={item} onPress={handlePress} />;
-    },
-    [router]
-  );
-
-  const renderHeader = useCallback(() => {
-    return (
-      <>
-        <HeaderComponent profile={profile} />
-        <Text size="xl" bold className="bg-background-50 px-4 py-2">
-          Posts
-        </Text>
-      </>
-    );
-  }, [HeaderComponent, profile]);
-
-  const renderListEmpty = useCallback(() => {
-    return (
-      <VStack space="lg" className="items-center justify-center p-5">
-        <Text className="mt-10 text-typography-700">
-          {isMeUser ? "You haven't posted anything yet." : 'This user has not posted anything yet.'}
-        </Text>
-        {isMeUser && (
-          <Link asChild push href={{ pathname: '/feed/create' }}>
-            <Button>
-              <ButtonText>Create Post</ButtonText>
-            </Button>
-          </Link>
-        )}
-      </VStack>
-    );
-  }, [isMeUser]);
+  const refreshing = isRefreshing || postsQuery.isRefetching;
+  const handleRefresh = useCallback(() => {
+    onRefresh?.();
+    postsQuery.refetch();
+  }, [onRefresh, postsQuery]);
 
   return (
-    <FlashList
+    <InfiniteFlashList
+      {...postsQuery}
+      gap={4}
       data={posts}
-      renderItem={renderItem}
+      contentContainerClassName="pb-4"
+      refreshing={refreshing}
+      onRefresh={handleRefresh}
       keyExtractor={listKeyExtractor}
-      showsVerticalScrollIndicator={false}
-      ListHeaderComponent={renderHeader}
-      ListEmptyComponent={renderListEmpty}
-      ListFooterComponent={() =>
-        postsQuery.isFetchingNextPage && <Spinner size={'small'} className="my-4" />
+      renderItem={({ item, isPlaceholder }) => (
+        <RenderItem item={item} isPlaceholder={isPlaceholder} />
+      )}
+      ListEmptyComponent={<EmptyState isMeUser={isMeUser} />}
+      ListHeaderComponent={
+        <ListHeader>
+          <HeaderComponent profile={profile} />
+        </ListHeader>
       }
-      contentContainerStyle={{ paddingBottom: 16 }}
-      ItemSeparatorComponent={() => <Box className="h-1" />}
-      refreshControl={<RefreshControl refreshing={isRefreshing ?? false} onRefresh={onRefresh} />}
-      onEndReached={postsQuery.fetchNextPage}
-      onEndReachedThreshold={0.25}
     />
+  );
+}
+
+function ListHeader({ children }: PropsWithChildren) {
+  return (
+    <>
+      {children}
+      <Text size="xl" bold className="bg-background-50 px-4 py-2">
+        Posts
+      </Text>
+    </>
+  );
+}
+
+function RenderItem({ item, isPlaceholder }: { item: Post; isPlaceholder: boolean }) {
+  const router = useRouter();
+  const handlePress = () => router.push(`/feed/${item.id}`);
+
+  if (isPlaceholder) return <PostPlaceholderItem />;
+  return <PostItem post={item} onPress={handlePress} />;
+}
+
+function EmptyState({ isMeUser }: { isMeUser: boolean }) {
+  return (
+    <VStack space="lg" className="items-center justify-center p-5">
+      <Text className="mt-10 text-typography-700">
+        {isMeUser ? "You haven't posted anything yet." : 'This user has not posted anything yet.'}
+      </Text>
+      {isMeUser && (
+        <Link asChild push href={{ pathname: '/feed/create' }}>
+          <Button>
+            <ButtonText>Create Post</ButtonText>
+          </Button>
+        </Link>
+      )}
+    </VStack>
   );
 }
