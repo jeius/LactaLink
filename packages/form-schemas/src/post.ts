f178f9dcfe3a78118030dbf87f4361805ee707d6
Julius Pahama <pahamajulius@gmail.com>
2026-04-27 11:52:15 +0800

    refactor(post-schema): update optional fields to use nullish for better validation handling

--- packages/form-schemas/src/post.ts
+++ packages/form-schemas/src/post.ts
@@ -4,20 +4,20 @@ import { emptyTransform } from './transformers';
 
 const mediaSchema = z.object({
   image: z.object(imageSchema.shape, 'Image is required'),
-  caption: z.string().transform(emptyTransform).optional(),
+  caption: z.string().transform(emptyTransform).nullish(),
 });
 
 export const postSchema = z.object({
   title: z.string('Title is required').nonempty('Title is required'),
-  content: z.string().transform(emptyTransform).optional(),
-  tags: z.array(z.string()).optional(),
-  media: z.array(mediaSchema).optional(),
+  content: z.string().transform(emptyTransform).nullish(),
+  tags: z.array(z.string()).nullish(),
+  media: z.array(mediaSchema).nullish(),
   sharedFrom: z
     .object({
       relationTo: z.enum(['posts', 'donations', 'requests']),
       value: z.string(),
     })
-    .optional(),
+    .nullish(),
 });
 
 export type PostSchema = z.infer<typeof postSchema>;
