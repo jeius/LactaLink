eca341c2f15287c71d4175805e3599da7ff689a7
Julius Pahama <pahamajulius@gmail.com>
2026-05-05 09:43:39 +0800

    refactor(AccountDeliveriesScreen): replace VerticalInfiniteList with InfiniteFlashList

--- apps/mobile/components/screens/transactions/AccountDeliveriesScreen.tsx
+++ apps/mobile/components/screens/transactions/AccountDeliveriesScreen.tsx
@@ -1,18 +1,17 @@
-import { VerticalInfiniteList } from '@/components/ui/list';
+import { InfiniteFlashList } from '@/components/ui/list';
 import { Skeleton } from '@/components/ui/skeleton';
 import TransactionListItem from '@/features/transactions/components/TransactionListItem';
 import { useInfiniteDeliveries } from '@/features/transactions/hooks/queries';
 import { useMeUser } from '@/hooks/auth/useAuth';
 import { isPlaceHolderData } from '@lactalink/utilities/checkers';
 import { Link } from 'expo-router';
-import React from 'react';
 
 export default function AccountDeliveriesScreen() {
   const { data, ...query } = useInfiniteDeliveries();
   const { data: meUser = null } = useMeUser();
 
   return (
-    <VerticalInfiniteList
+    <InfiniteFlashList
       {...query}
       data={data}
       gap={14}
