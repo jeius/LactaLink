fcc4cc75d66577d9463638ac8b11bf9463934e72
Julius Pahama <pahamajulius@gmail.com>
2026-05-06 10:02:35 +0800

    refactor(ProfileCard): simplify imports and remove unused variables

--- apps/mobile/components/cards/ProfileCard.tsx
+++ apps/mobile/components/cards/ProfileCard.tsx
@@ -1,23 +1,20 @@
-import { ProfileAvatar } from '@/components/Avatar';
+import Avatar from '@/components/Avatar';
 import { Button, ButtonText } from '@/components/ui/button';
-import { Card } from '@/components/ui/card';
+import { Card, CardProps } from '@/components/ui/card';
 import { HStack } from '@/components/ui/hstack';
 import { Icon } from '@/components/ui/icon';
 import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
 import { useProfileData } from '@/features/profile/hooks/useProfileData';
-import { PROFILE_TYPE_ICONS } from '@/features/profile/lib/constants';
-import { getColor } from '@/lib/colors';
 import { User } from '@lactalink/types/payload-generated-types';
 import { extractCollection, extractDisplayName, extractID } from '@lactalink/utilities/extractors';
 import { useRouter } from 'expo-router';
 import { MailIcon, PhoneIcon } from 'lucide-react-native';
-import React, { ComponentProps, ReactNode } from 'react';
-import { Box } from '../ui/box';
+import { ReactNode } from 'react';
 import { Pressable } from '../ui/pressable';
 import { Skeleton } from '../ui/skeleton';
 
-interface ProfileCardProps extends ComponentProps<typeof Card> {
+interface ProfileCardProps extends CardProps {
   profile: NonNullable<User['profile']>;
   isLoading?: boolean;
   action?: ReactNode;
@@ -30,7 +27,6 @@ export default function ProfileCard({
   isLoading: isLoadingProp,
   variant = 'ghost',
   action,
-  hideBadge = false,
   disableNavigation = false,
   ...cardProps
 }: ProfileCardProps) {
@@ -46,8 +42,6 @@ export default function ProfileCard({
   const name = (data && extractDisplayName({ profile: data })) || 'User';
   const email = user?.email || 'No email';
   const phone = profile?.phone || 'No phone number';
-  const profileIcon =
-    (user?.profileType && PROFILE_TYPE_ICONS[user.profileType]) || PROFILE_TYPE_ICONS.INDIVIDUAL;
 
   const navigateToProfile = () => {
     if (disableNavigation) return;
@@ -65,12 +59,7 @@ export default function ProfileCard({
             pointerEvents={disableNavigation ? 'none' : 'auto'}
             onPress={navigateToProfile}
           >
-            <ProfileAvatar profile={profileProp} size="xl" />
-            {profileIcon && !hideBadge && (
-              <Box className="absolute right-1 top-0 rounded-full bg-background-0 p-1.5">
-                <Icon as={profileIcon} size="xs" color={getColor('typography', '700')} />
-              </Box>
-            )}
+            <Avatar profile={profileProp} size="xl" />
           </Pressable>
 
           <VStack space="sm" className="flex-1 items-start">
@@ -82,26 +71,21 @@ export default function ProfileCard({
               onPress={navigateToProfile}
               pointerEvents={disableNavigation ? 'none' : 'auto'}
             >
-              <ButtonText
-                underlineOnPress
-                ellipsizeMode="tail"
-                numberOfLines={1}
-                className="flex-1"
-                size="md"
-              >
+              <ButtonText numberOfLines={1} className="flex-1" size="md">
                 {name}
               </ButtonText>
             </Button>
+
             <HStack space="sm" className="items-center">
               <Icon as={MailIcon} size="sm" />
-              <Text size="sm" ellipsizeMode="tail" numberOfLines={1} className="flex-1">
+              <Text size="sm" numberOfLines={1} className="flex-1">
                 {email}
               </Text>
             </HStack>
 
             <HStack space="sm" className="items-center">
               <Icon as={PhoneIcon} size="sm" />
-              <Text size="sm" ellipsizeMode="tail" numberOfLines={1} className="flex-1">
+              <Text size="sm" numberOfLines={1} className="flex-1">
                 {phone}
               </Text>
             </HStack>
