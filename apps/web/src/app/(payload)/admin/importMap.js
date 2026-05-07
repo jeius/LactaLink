bff3c1a5ad4e2ebea84f1e1dc0ad92f2d68614ca
Julius Pahama <pahamajulius@gmail.com>
2026-05-07 17:09:06 +0800

    chore(web): regenerate types and importMap

--- apps/web/src/app/(payload)/admin/importMap.js
+++ apps/web/src/app/(payload)/admin/importMap.js
@@ -15,6 +15,7 @@ import { default as default_cfbe37a33e0e14fa5ae8f22a04bbe4da } from '@/component
 import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
+/** @type import('payload').ImportMap */
 export const importMap = {
   "@payloadcms/plugin-search/client#LinkToDoc": LinkToDoc_aead06e4cbf6b2620c5c51c9ab283634,
   "@payloadcms/plugin-search/client#ReindexButton": ReindexButton_aead06e4cbf6b2620c5c51c9ab283634,
